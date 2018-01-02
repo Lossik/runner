413001faf18048e0547e9c77a5fcd499cfcc27d2
Petr Losse <losse@websoft.cz>
2018-01-02 16:11:30 +0100

    commit

--- src/Runner.php
+++ src/Runner.php
@@ -25,6 +25,12 @@ class Runner
 	}
 
 
+	public function getProcessors()
+	{
+		return $this->settings ? array_keys($this->settings) : [];
+	}
+
+
 	public function Run($processorName = null)
 	{
 		$runn = $processorName ?
@@ -39,7 +45,7 @@ class Runner
 				$processor->process($parameters);
 			}
 			else {
-				$parameters = $this->getParameters($sourceData, isset($setting['parameters']) ? $setting['parameters'] : []);
+				$parameters = $this->getParameters($sourceData, self::ifset($setting, 'parameters', []));
 				$processor->{$setting['processor']['call']}($parameters);
 			}
 		}
@@ -48,34 +54,22 @@ class Runner
 
 	protected function createProcessor($settingProcessor)
 	{
-		$processor = $this->create(self::ifset($settingProcessor, 'class'), self::ifset($settingProcessor, 'setup', []));
+		$service = self::ifset($settingProcessor, 'service', false);
+		if ($service) {
+			return $this->di->createService($service);
+		}
+		$class     = self::ifset($settingProcessor, 'class');
+		$setup     = self::ifset($settingProcessor, 'setup', []);
+		$processor = $this->create($class, $setup);
 		if ($processor instanceof IProcessor) {
-			foreach ($processor->getParametersClasses() as $class) {
-				$parameter = $this->create($class, [], IParameter::class);
+			foreach ($processor->getParametersClasses() as $parametersClass) {
+				$parameter = $this->create($parametersClass, [], IParameter::class);
 				$processor->setupParameter($parameter);
 				$this->registerParameter($parameter);
 			}
 		}
 
 		return $processor;
-	}
-
-
-	protected function create($class, $setup = [], $instanceof = null)
-	{
-		var_dump($class);
-		$object = new $class;
-		if ($instanceof) {
-			if (!($object instanceof $instanceof)) {
-				throw new \LogicException();
-			}
-		}
-		InjectExtension::callInjects($this->di, $object);
-		foreach ($setup as list($method, $arguments)) {
-			$object->{$method}(...$arguments);
-		}
-
-		return $object;
 	}
 
 
@@ -94,6 +88,23 @@ class Runner
 	}
 
 
+	protected function create($class, $setup = [], $instanceof = null)
+	{
+		$object = new $class;
+		if ($instanceof) {
+			if (!($object instanceof $instanceof)) {
+				throw new \LogicException();
+			}
+		}
+		InjectExtension::callInjects($this->di, $object);
+		foreach ($setup as list($method, $arguments)) {
+			$object->{$method}(...$arguments);
+		}
+
+		return $object;
+	}
+
+
 	public function registerParameter(IParameter $parameter)
 	{
 		$this->parameters[$parameter->getName()] = $parameter;
@@ -102,8 +113,10 @@ class Runner
 
 	protected function getSourceData($settingSource)
 	{
+		$class = self::ifset($settingSource, 'class', AbstractSource::class);
+		$setup = self::ifset($settingSource, 'setup', []);
 		/** @var ISource $source */
-		$source = $this->create(self::ifset($settingSource, 'class'), self::ifset($settingSource, 'setup', []), ISource::class);
+		$source = $this->create($class, $setup, ISource::class);
 
 		return $source->getSourceData();
 	}
@@ -115,12 +128,27 @@ class Runner
 		foreach ($parameters as $parameter) {
 			$parameterName = $this->parameters[$parameter]->getName();
 			$items         = $this->parameters[$parameter]->getParametersItems($sourceData);
-			foreach ($items as $contract_id => $item) {
-				$result[$contract_id][$parameterName] = $item;
+			foreach ($items as $parameter_id => $item) {
+				$result[$parameter_id][$parameterName] = $item;
 			}
 		}
 
 		return $result;
+	}
+
+
+	public function RunWidthSourceData($processorName, $sourceData)
+	{
+		$setting   = $this->settings[$processorName];
+		$processor = $this->createProcessor($setting['processor']);
+		if ($processor instanceof IProcessor) {
+			$parameters = $this->getParameters($sourceData, $processor->getParameters());
+			$processor->process($parameters);
+		}
+		else {
+			$parameters = $this->getParameters($sourceData, self::ifset($setting, 'parameters', []));
+			$processor->{self::ifset($setting, 'processor/call')}($parameters);
+		}
 	}
 
 }
